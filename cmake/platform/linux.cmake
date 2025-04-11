aa5068a02fd424c8c6036bd250a19aa565b964cf
Wend4r <47463683+Wend4r@users.noreply.github.com>
2025-04-11 09:36:05 +0300

    CMake (Linux): add SSE4.2 support

--- cmake/platform/linux.cmake
+++ cmake/platform/linux.cmake
@@ -15,7 +15,7 @@ set(PLATFORM_COMPILE_OPTIONS
 	-Wno-unused -Wno-register
 	-Wno-narrowing
 
-	-mtune=generic -mmmx -msse -msse2 -msse3 -msse4 -msse4.1
+	-mtune=generic -mmmx -msse -msse2 -msse3 -msse4 -msse4.1 -msse4.2
 	-fvisibility=default -fPIC
 
 	-fno-strict-aliasing -fno-threadsafe-statics
